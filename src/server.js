01d6ef03194fac13c19e99775b49cded9d4b6f9b
victorfeldhaus <victor.feldhaus@hotmail.com>
2020-09-13 22:57:28 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require("./pages")
+
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+})
+
+server.use(express.urlencoded({ extended: true}))
+server.use(express.static("public"))
+
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes",pageGiveClasses)
+.post("/save-classes", saveClasses)
+
+.listen(5500)
